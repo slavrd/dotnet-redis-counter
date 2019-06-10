d3e94cf086deaf38258ae3734dd2fda8052ef125
Slav Donchev <slav.donchev@gmail.com>
2019-06-10 15:47:05 +0300

    add dotnet install script

---
+++ scripts/dotnet_setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Installs .Net Core SDK
+sudo apt-get update
+
+wget -q -P /tmp https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+sudo dpkg -i /tmp/packages-microsoft-prod.deb
+
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-2.2
